ba4b2c56c3ef1c170ed9832e946bdb3ec06fddc5
duckhee <fain9301@yahoo.com>
2020-03-24 01:01:10 +0900

    feat:database test

---
+++ Board-Mybatis/src/test/java/kr/co/portfolio/daoTests/ConnectionTests.java
@@ -0,0 +1,62 @@
+package kr.co.portfolio.daoTests;
+
+import static org.junit.Assert.fail;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations= {"file:src/main/webapp/WEB-INF/spring/root-context.xml"})
+public class ConnectionTests {
+	
+	@Setter(onMethod_ = @Autowired)
+	private DataSource dataSource;
+	
+	@Setter(onMethod_ = @Autowired)
+	private SqlSessionFactory sqlSessionFactory;
+
+	@Test
+	public void testConnection() {
+		try(Connection con = dataSource.getConnection()){
+			log.info("Connection : " + con);
+		}catch (Exception e) {
+			// TODO: handle exception
+			fail(e.getMessage());
+		}
+	}
+	
+	@Test
+	public void JDBCTests() {
+		try(Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/spring_study?useSSL=false&serverTimezone=UTC", "root","won1228A!")){
+			log.info("get Connection : " + con);
+		}catch (Exception e) {
+			// TODO: handle exception
+			fail(e.getMessage());
+		}
+	}
+	
+	@Test
+	public void MybatisTests() {
+		try(SqlSession sesson = sqlSessionFactory.openSession(); Connection con = sesson.getConnection()){
+			log.info("Get Mybatis Connection : " + con);
+		}catch (Exception e) {
+			// TODO: handle exception
+			fail(e.getMessage());
+		}
+	}
+	
+}
