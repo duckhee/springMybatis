5c96ef564eccce32b31d6beee411c4de3671378e
duckhee <fain9301@yahoo.com>
2020-04-05 16:47:11 +0900

    feat: reply Mapper Interface
    create mapper reply interface

--- Board-Mybatis/src/main/java/kr/co/portfolio/mapper/reply/ReplyMapper.java
+++ Board-Mybatis/src/main/java/kr/co/portfolio/mapper/reply/ReplyMapper.java
@@ -1,5 +1,17 @@
 package kr.co.portfolio.mapper.reply;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import kr.co.portfolio.vo.BoardReplyVO;
+import kr.co.portfolio.vo.Criteria;
+
 public interface ReplyMapper {
+	
+	/** Search Board reply Count */
+	public int getCountByBno(@Param("cri")Criteria cri, @Param("bno")Long bno);
+	/** Search Board reply list */
+	public List<BoardReplyVO> getListWithPaging(@Param("cri")Criteria cri, @Param("bno")Long bno);
 
 }
